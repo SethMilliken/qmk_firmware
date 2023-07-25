4582dfa94dab0b200afabfd4217650fc6fbd812a
Seth Milliken <seth_github@araxia.net>
2023-07-24 17:43:02 -0700

    1.40.1: fix combos after changing bindings on combo positions
    ZMK position-based combos are so much better than this bs.

--- keyboards/splitkb/aurora/corne/keymaps/SethMilliken/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/SethMilliken/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define CONFIG_VERSION "1.40"
+#define CONFIG_VERSION "1.40.1"
 
 #define HYPE(x) C(A(G(x)))
 
@@ -100,8 +100,8 @@ const uint16_t PROGMEM combo_tmux[]          = {KC_U                 ,  KC_I    
 const uint16_t PROGMEM combo_tmux_mode[]     = {KC_W                 ,  KC_E                   ,  KC_R         ,  COMBO_END};
 const uint16_t PROGMEM combo_del[]           = {KC_P                 ,  LT(_WIN_RIGHT,KC_BSPC) ,  COMBO_END};
 const uint16_t PROGMEM combo_gui_left[]      = {LALT_T(KC_GRV)       ,  KC_Z                   ,  COMBO_END};
-const uint16_t PROGMEM combo_alt_left[]      = {LCTL_T(KC_ESC)       ,  LALT_T(KC_GRV)         ,  COMBO_END};
-const uint16_t PROGMEM combo_gui_right[]     = {KC_SLSH              ,  RALT_T(KC_BSLS)        ,  COMBO_END};
+const uint16_t PROGMEM combo_alt_left[]      = {LCTL_T(KC_ENT)       ,  LALT_T(KC_GRV)         ,  COMBO_END};
+const uint16_t PROGMEM combo_gui_right[]     = {KC_GRV               ,  RALT_T(KC_BSLS)        ,  COMBO_END};
 const uint16_t PROGMEM combo_alt_right[]     = {RCTL_T(KC_QUOT)      ,  RALT_T(KC_BSLS)        ,  COMBO_END};
 const uint16_t PROGMEM combo_inverse_winl[]  = {LT(_WIN_LEFT,KC_TAB) ,  LT(_SYMBOLS,KC_ESC)    ,  COMBO_END};
 const uint16_t PROGMEM combo_inverse_winr[]  = {LT(_MEDIA,KC_ESC)    ,  LT(_WIN_RIGHT,KC_BSPC) ,  COMBO_END};
