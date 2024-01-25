03ac582939e882edb82ccdeaf83afc82856e7391
Seth Milliken <seth_github@araxia.net>
2024-01-25 15:00:20 -0800

    2.00.0: add support for modal layers
    - bindings to activate modal layers
    - send keys to activate mode card
    - send keys to deactivate mode card
    - rearrange layers to work correctly with modal layer
    - adjustments to sync discord layer with zmk

--- keyboards/splitkb/aurora/corne/keymaps/SethMilliken/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/SethMilliken/keymap.c
@@ -5,8 +5,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_BASE] = LAYOUT_split_3x6_3(
 XXXXXXX ,  LT(_WIN_LEFT,KC_Q) ,  KC_W         ,  KC_E           ,  KC_R              ,  KC_T                ,    KC_Y              ,  KC_U               ,  KC_I             ,  KC_O         ,  LT(_WIN_RIGHT,KC_P) , XXXXXXX ,
-XXXXXXX ,  LCTL_T(KC_A)       ,  LGUI_T(KC_S) ,  LALT_T(KC_D)   ,  LSFT_T(KC_F)      ,  KC_G                ,    KC_H              ,  RSFT_T(KC_J)       ,  LALT_T(KC_K)     ,  RGUI_T(KC_L) ,  RCTL_T(KC_SCLN)     , XXXXXXX ,
-XXXXXXX ,  KC_Z               ,  KC_X         ,  KC_C           ,  KC_V              ,  KC_B                ,    KC_N              ,  KC_M               ,  KC_COMMA         ,  KC_DOT       ,  KC_SLSH             , XXXXXXX ,
+XXXXXXX ,  LCTL_T(KC_A)       ,  LGUI_T(KC_S) ,  LALT_T(KC_D)   ,  LSFT_T(KC_F)      ,  KC_G                ,    KC_H              ,  RSFT_T(KC_J)       ,  LALT_T(KC_K)     ,  RGUI_T(KC_L) ,  RCTL_T(KC_SCLN)     , POINTER_ON ,
+XXXXXXX ,  KC_Z               ,  KC_X         ,  KC_C           ,  KC_V              ,  KC_B                ,    KC_N              ,  KC_M               ,  KC_COMMA         ,  KC_DOT       ,  KC_SLSH             , DISCORD_ON ,
                                                  LGUI_T(KC_ENT) ,  LT(_LNUM,C(KC_W)) ,  LT(_SYMBOLS,KC_ESC) ,    LT(_MEDIA,KC_ESC) ,  LT(_NUMPAD,KC_SPC) ,  RGUI_T(KC_COLON)
         ),
         [_SYMBOLS] = LAYOUT_split_3x6_3(
@@ -33,6 +33,25 @@ XXXXXXX ,  KC_F6   ,  KC_F7   ,  KC_F8   ,  KC_F9   ,  KC_F10  ,  KC_VOLD ,  KC_
 XXXXXXX ,  KC_F11  ,  KC_F12  ,  KC_F13  ,  KC_F14  ,  KC_F15  ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
                                  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS
         ),
+        // tri-state layer; do not switch to directly
+        [_ADJUST] = LAYOUT_split_3x6_3(
+XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,    XXXXXXX ,  CW_TOGG    ,  XXXXXXX  ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
+XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  WARPD_L ,    WARPD_R ,  WARPD_MOV  ,  WARPD_2P ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
+XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  KC_GRV  ,    KC_QUOT ,  S(KC_QUOT) ,  XXXXXXX  ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
+                                 KC_TRNS ,  KC_TRNS ,  KC_TRNS ,    KC_TRNS ,  KC_TRNS    ,  KC_TRNS
+        ),
+        [_DISCORD] = LAYOUT_split_3x6_3(
+XXXXXXX ,  XXXXXXX ,  KC_TAB  ,  KC_UP   ,  KC_PLUS  ,  DIS_SECT_NAV ,      XXXXXXX      ,  DIS_PREV_SERV ,  DIS_NEXT_SERV ,  DIS_PINS      ,  ACT_RIGHT     , XXXXXXX ,
+XXXXXXX ,  XXXXXXX ,  KC_LEFT ,  KC_DOWN ,  KC_RIGHT ,  XXXXXXX      ,      KC_PGDN      ,  DIS_NEXT      ,  DIS_PREV      ,  DIS_PREV_CHAN ,  DIS_NEXT_CHAN , XXXXXXX ,
+XXXXXXX ,  XXXXXXX ,  KC_HOME ,  KC_ESC  ,  KC_END   ,  XXXXXXX      ,      KC_PGUP      ,  DIS_PREV_HIST ,  DIS_NEXT_HIST ,  XXXXXXX       ,  DIS_MENT      , XXXXXXX ,
+                                 KC_TRNS ,  KC_TRNS  ,  MODE_OFF     ,      MODE_OFF     ,  KC_TRNS       ,  KC_TRNS
+        ),
+        [_POINTER] = LAYOUT_split_3x6_3(
+XXXXXXX ,  KC_ACL0 ,  KC_BTN1 ,  KC_MS_U ,  KC_BTN2 ,  KC_WH_U  ,    XXXXXXX  ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
+XXXXXXX ,  KC_ACL1 ,  KC_MS_L ,  KC_MS_D ,  KC_MS_R ,  KC_WH_D  ,    KC_MS_L  ,  KC_MS_D ,  KC_MS_U ,  KC_MS_R ,  XXXXXXX , XXXXXXX ,
+XXXXXXX ,  KC_ACL2 ,  WARPD_L ,  WARPD_T ,  WARPD_R ,  XXXXXXX  ,    KC_BTN1  ,  KC_BTN2 ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
+                                 KC_TRNS ,  KC_TRNS ,  MODE_OFF ,    MODE_OFF ,  KC_TRNS ,  KC_TRNS
+        ),
         [_WIN_LEFT] = LAYOUT_split_3x6_3(
 XXXXXXX ,  KC_TRNS      ,  LGUI(KC_LBRC) ,  LGUI(KC_RBRC) ,  SGUI(KC_LCBR) ,  SGUI(KC_RCBR)     ,  RCS(KC_Y)  ,  RCS(KC_U) ,  RCS(KC_I)  ,  RCS(KC_O)  ,  LT(_WIN_RIGHT,RCS(KC_P)) , XXXXXXX ,
 XXXXXXX ,  LGUI(KC_GRV) ,  G(C(KC_4))    ,  G(C(KC_3))    ,  G(C(KC_2))    ,  G(C(KC_1))        ,  RCS(KC_H)  ,  RCS(KC_J) ,  RCS(KC_K)  ,  RCS(KC_L)  ,  RCS(KC_COLON)            , XXXXXXX ,
@@ -46,31 +65,11 @@ XXXXXXX ,                RCS(KC_Z) ,  RCS(KC_X) ,  RCS(KC_C) ,  RCS(KC_V) ,  RCS
                                                    KC_TRNS   ,  KC_TRNS   ,  KC_TRNS         ,  KC_TRNS       ,  KC_TRNS       ,  KC_TRNS
         ),
         // tri-state layer; do not switch to directly
-        [_ADJUST] = LAYOUT_split_3x6_3(
-XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,    XXXXXXX ,  CW_TOGG    ,  XXXXXXX  ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
-XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  WARPD_L ,    WARPD_R ,  WARPD_MOV  ,  WARPD_2P ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
-XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  KC_GRV  ,    KC_QUOT ,  S(KC_QUOT) ,  XXXXXXX  ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
-                                 KC_TRNS ,  KC_TRNS ,  KC_TRNS ,    KC_TRNS ,  KC_TRNS    ,  KC_TRNS
-        ),
-        // tri-state layer; do not switch to directly
         [_LIGHTING] = LAYOUT_split_3x6_3(
 XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  RGB_VAI ,  RGB_VAD ,  RGB_TOG ,  QK_BOOT ,  QK_RBT     ,  KC_F18 ,  KC_F17 ,  KC_F16 , XXXXXXX ,
 XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  RGB_SAI ,  RGB_SAD ,  RGB_MOD ,  DB_TOGG ,  XXXXXXX    ,  KC_F21 ,  KC_F20 ,  KC_F19 , XXXXXXX ,
 XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  RGB_HUI ,  RGB_HUD ,  CW_TOGG ,  QK_MAKE ,  S(QK_MAKE) ,  KC_F24 ,  KC_F23 ,  KC_F22 , XXXXXXX ,
                                  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS ,  KC_TRNS    ,  KC_TRNS
-        ),
-        // tri-state layer; do not switch to directly
-        [_DISCORD] = LAYOUT_split_3x6_3(
-XXXXXXX ,  XXXXXXX ,  KC_TAB  ,  KC_UP   ,  KC_PLUS  ,  XXXXXXX   ,      DF(_DISCORD) ,  DIS_PREV_SERV ,  DIS_NEXT_SERV ,  DIS_PINS   ,  C(S(KC_N)) , XXXXXXX ,
-XXXXXXX ,  XXXXXXX ,  KC_LEFT ,  KC_DOWN ,  KC_RIGHT ,  XXXXXXX   ,      KC_PGDN      ,  DIS_NEXT      ,  DIS_PREV      ,  KC_PGUP    ,  KC_PGDN    , XXXXXXX ,
-XXXXXXX ,  XXXXXXX ,  KC_HOME ,  KC_ESC  ,  KC_END   ,  XXXXXXX   ,      KC_PGUP      ,  DIS_PREV_HIST ,  DIS_NEXT_HIST ,  XXXXXXX    ,  DIS_MENT   , XXXXXXX ,
-                                 KC_TRNS ,  KC_TRNS  ,  DF(_BASE) ,      DF(_BASE)    ,  KC_TRNS       ,  KC_TRNS
-        ),
-        [_POINTER] = LAYOUT_split_3x6_3(
-XXXXXXX ,  KC_ACL0 ,  KC_BTN1 ,  KC_MS_U ,  KC_BTN2 ,  KC_WH_U ,    XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
-XXXXXXX ,  KC_ACL1 ,  KC_MS_L ,  KC_MS_D ,  KC_MS_R ,  KC_WH_D ,    KC_MS_L ,  KC_MS_D ,  KC_MS_U ,  KC_MS_R ,  XXXXXXX , XXXXXXX ,
-XXXXXXX ,  KC_ACL2 ,  WARPD_L ,  WARPD_T ,  WARPD_R ,  XXXXXXX ,    KC_BTN1 ,  KC_BTN2 ,  XXXXXXX ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,
-                                 KC_TRNS ,  KC_TRNS ,  KC_TRNS ,    KC_TRNS ,  KC_TRNS ,  KC_TRNS
         )
 };
 // }}}
